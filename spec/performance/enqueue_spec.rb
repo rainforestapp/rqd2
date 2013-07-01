ff04b5fcabb6e39ac40645c0284bc2d5e29a04cf
Russell Smith <russell.smith@ukd1.co.uk>
2013-07-01 14:44:13 -0700

    Basic 10k test

---
+++ spec/performance/enqueue_spec.rb
@@ -0,0 +1,27 @@
+require 'benchmark'
+
+describe Rqd2 do
+  context "10k test" do
+    before do
+      puts Benchmark.measure {
+        10_000.times do
+          Rqd2.enqueue MyJob, 1, 2, 3
+        end
+      }
+    end
+
+    it "enqueues 10000 jobs" do
+      Rqd2.size.should == 10_000
+    end
+
+    it "denqueues 10000 jobs" do
+      puts Benchmark.measure {
+        10_000.times do
+          Rqd2.dequeue
+        end
+      }
+
+      Rqd2.size.should == 0
+    end
+  end
+end
